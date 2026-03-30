bb36211d298dcc5329390789fbf34032db950125
Osva1 <osvelcholo17@gmail.com>
2026-03-30 12:40:21 -0600

    añadir archivos faltantes

--- src/models/user.js
+++ src/models/user.js
@@ -4,6 +4,7 @@ const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   lastName: { type: String, required: true },
   age: { type: Number, required: true },
+  ingresos: { type: Number, required: true},
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   tienePatio: { type: Boolean, default: false },  
